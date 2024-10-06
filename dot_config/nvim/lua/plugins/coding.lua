dc5b483a82ab3633dd1bada543e8a29da29c1caf
Milton Siqueira <miltonsiqueira@gmail.com>
2024-10-06 17:35:06 +0300

    neovim - add more plugins and TAB setup for autocompletion

---
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      })
+      return opts
+    end,
+  },
+}
